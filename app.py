0ce0a07649b97f93cf2ce1420d00a74005fa16f4
Zhiyuan Li <zyuanlee@cumt.edu.cn>
2020-12-10 22:31:52 +0800

    update

--- app.py
+++ app.py
@@ -1,120 +1,49 @@
-# coding:utf-8
-
-import os
-from docx import Document
-from docx.shared import Pt
-import logging
-import time
 from wxpay import *
-from flask import Flask, request, jsonify, current_app
-import socket
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
-app.config['JSON_AS_ASCII'] = False
-
-# 获取本机计算机名称s
-hostname = socket.gethostname()
-socket.gethostbyname(hostname)
-app = Flask(__name__)
-appid = 'wxbcdbe97d7b353c80'  # 小程序ID
-mch_id = '1111111111'  # 商户号，修改为自己的
-total_fee = '5000'  # 总金额
-attach = 'test'
-# 获取本机ip
-spbill_create_ip = socket.gethostbyname(hostname)  # 终端IP
-notify_url = 'https://file.cumtlee.cn/wxpay/notify'  # 通知地址
-trade_type = 'JSAPI'  # 交易类型
-merchant_key = 'FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF'  # 商户KEY，修改为自己的
 
 
 @app.route('/')
 def index():
-    '''
-    :return:
-    '''
-    return jsonify({'errcode': 0, 'errmsg': 'ok'})
+    return jsonify({'code': 0, 'msg': 'success'})
 
 
 @app.route('/wxpay/pay')
 def create_pay():
-    '''
+    """
     请求支付
+    https://pay.weixin.qq.com/wiki/doc/api/wxa/wxa_api.php?chapter=9_1&index=1
     :return:
-    '''
-    data = {
-        'appid': appid,
-        'mch_id': mch_id,
-        'attach': attach,
-        'nonce_str': get_nonce_str(),
-        'body': 'JSAPI-Pay',  # 商品描述
-        'out_trade_no': str(int(time.time())),  # 商户订单号
-        'total_fee': total_fee,
-        'spbill_create_ip': spbill_create_ip,
-        'notify_url': notify_url,
-        'trade_type': trade_type,
-        'openid': request.args.get('openid')
+    """
+    pay_data = {
+        'body': '支付-测试',  # 商品描述
+        'attach': '附加数据',  # 附加数据
+        'total_fee': '1',  # 订单总金额，单位为分
+        'openid': request.args.get('openid') or "o0wGL5cdilBxYCYc_VPKqvJypB1M"
+
     }
-    '''
-    生成签名
-    :return:
-    '''
-    # print(data)
-    wxpay = WxPay(merchant_key, data)
+    wxpay = WxPay(pay_data)
     pay_info = wxpay.get_pay_info()
     if pay_info:
         return jsonify(pay_info)
-    return jsonify({'errcode': 40001, 'errmsg': '请求支付失败'})
+    return jsonify({'code': 40001, 'msg': '请求支付失败'})
 
 
 @app.route('/wxpay/notify', methods=['POST'])
-def wxpay():
-    '''
+def notify():
+    """
     支付回调通知
     :return:
-    '''
+    """
     if request.method == 'POST':
-        logging.info(xml_to_dict(request.data))
         result_data = {
             'return_code': 'SUCCESS',
             'return_msg': 'OK'
         }
-        return dict_to_xml(result_data), {'Content-Type': 'application/xml'}
-
-
-@app.route("/downfile/<filename>")
-def getfile(filename):
-    '''
-    生成准考证
-    :return:
-    '''
-    param = request.args
-    # print(param)
-    document = Document(r"zkz.docx")
-    tables = document.tables
-    arr = [
-        [param.get('stu_name'), param.get('exam_time')],
-        [param.get('stu_college'), param.get('exam_site')],
-        [param.get('stu_id'), param.get('exam_location')],
-        [param.get('stu_school'), param.get('exam_seat_num')],
-        [param.get('stu_catetory'), param.get('exam_zkz_num')]
-    ]
-    for i in range(5):
-        tables[0].cell(i + 1, 1).paragraphs[0].add_run(arr[i][0]).font.size = Pt(14)
-        tables[0].cell(i + 1, 3).paragraphs[0].add_run(arr[i][1]).font.size = Pt(14)
-
-    path = './files/' + param.get('stu_id') + '.docx'
-    document.save(path)
-
-    def generate():
-        with open(path, 'rb') as f:
-            yield from f
-        os.remove(path)
-
-    r = current_app.response_class(generate(),
-                                   mimetype='application/vnd.openxmlformats-officedocument.wordprocessingml.document')
-    exam_zkz_num = param.get('exam_zkz_num') + '.docx'
-    r.headers.set('Content-Disposition', 'attachment', filename=exam_zkz_num)
-    return r
+        return dict_to_order_xml(result_data), {'Content-Type': 'application/xml'}
+    else:
+        return 'error'
 
 
 if __name__ == '__main__':
--- wxpay.py
+++ wxpay.py
@@ -1,3 +1,5 @@
+import socket
+import time
 import uuid
 import hashlib
 import xml.etree.ElementTree as ET
@@ -5,32 +7,33 @@ import requests
 
 
 def get_nonce_str():
-    '''
+    """
     获取随机字符串
     :return:
-    '''
+    """
     return str(uuid.uuid4()).replace('-', '')
 
 
-def dict_to_xml(dict_data):
-    '''
-    dict to xml
+def dict_to_order_xml(dict_data):
+    """
+    dict to order xml
+    ASCII码从小到大排序
     :param dict_data:
     :return:
-    '''
+    """
     xml = ["<xml>"]
-    for k, v in dict_data.items():
-        xml.append("<{0}>{1}</{0}>".format(k, v))
+    for k in sorted(dict_data):
+        xml.append("<{0}>{1}</{0}>".format(k, dict_data.get(k)))
     xml.append("</xml>")
     return "".join(xml)
 
 
 def xml_to_dict(xml_data):
-    '''
+    """
     xml to dict
     :param xml_data:
     :return:
-    '''
+    """
     xml_dict = {}
     root = ET.fromstring(xml_data)
     for child in root:
@@ -39,77 +42,86 @@ def xml_to_dict(xml_data):
 
 
 class WxPay(object):
+    """
+    https://pay.weixin.qq.com/wiki/doc/api/wxa/wxa_api.php?chapter=7_4&index=3
+    """
 
-    def __init__(self, merchant_key, kwargs):
+    def __init__(self, pay_data):
         self.url = 'https://api.mch.weixin.qq.com/pay/unifiedorder'
-        self.merchant_key = merchant_key
-        self.pay_data = kwargs
+        self.appid = 'wxbcdbe97d7b353c80'  # 微信分配的小程序ID
+        self.mch_id = '1526671931'  # 商户号
+        self.notify_url = 'https://xxxx/wxpay/notify'  # 通知地址
+        self.spbill_create_ip = '127.0.0.1' or socket.gethostbyname(socket.gethostname())  # 获取本机ip
+        self.merchant_key = 'lxwxx2014qgdxsyyjs2019wgyxy20190'  # 商户KEY，修改为自己的
+        self.pay_data = pay_data
 
     def create_sign(self, pay_data):
-        '''
+        """
         生成签名
+        https://pay.weixin.qq.com/wiki/doc/api/wxa/wxa_api.php?chapter=4_3
+        :param pay_data:
         :return:
-        '''
-        '''生成签名'''
-        stringA = ''
-        # ks = sorted(pay_data.keys())
-        # 参数排序
-        # for k in ks:
-        #     stringA += (k + '=' + pay_data[k] + '&')
-        # # 拼接商户KEY
-        # stringSignTemp = stringA + "key=" + self.merchant_key
-        # md5加密，也可以用其他方式
-        # hash_md5 = hashlib.md5(stringSignTemp.encode('utf8'))
-        # sign = hash_md5.hexdigest().upper()
-        # return sign
+        """
 
-        stringA = '&'.join(["{0}={1}".format(k, pay_data.get(k)) for k in sorted(pay_data)])
-        stringSignTemp = '{0}&key={1}'.format(stringA, self.merchant_key).encode('utf-8')
-        sign = hashlib.md5(stringSignTemp).hexdigest()
+        # 拼接stringA
+        string_a = '&'.join(["{0}={1}".format(k, pay_data.get(k)) for k in sorted(pay_data)])
+        # 拼接key
+        string_sign_temp = '{0}&key={1}'.format(string_a, self.merchant_key).encode('utf-8')
+        # md5签名
+        sign = hashlib.md5(string_sign_temp).hexdigest()
         return sign.upper()
 
     def get_pay_info(self):
-        '''
-        获取支付信息
-        :param xml_data:
+        """
+        支付统一下单
         :return:
-        '''
+        """
         # 调用签名函数
-        sign = self.create_sign(self.pay_data)
-        self.pay_data['sign'] = sign
-        # 拼接 XMl
-        xmlstr = '<xml>' \
-                 '<appid>wxbcdbe97d7b353c80</appid>' \
-                 '<attach>test</attach>' \
-                 '<body>JSAPI-Pay</body>' \
-                 '<mch_id>1526671931</mch_id>' \
-                 '<nonce_str>{nonce_str}</nonce_str>' \
-                 '<notify_url>https://file.cumtlee.cn/wxpay/notify</notify_url>' \
-                 '<openid>{openid}</openid>' \
-                 '<out_trade_no>{out_trade_no}</out_trade_no>' \
-                 '<spbill_create_ip>{spbill_create_ip}</spbill_create_ip>' \
-                 '<total_fee>5000</total_fee>' \
-                 '<trade_type>JSAPI</trade_type>' \
-                 '<sign>{sign}</sign>' \
-                 '</xml>'
-        xml = xmlstr.format(nonce_str=self.pay_data['nonce_str'],
-                            openid=self.pay_data['openid'],
-                            out_trade_no=self.pay_data['out_trade_no'],
-                            spbill_create_ip=self.pay_data['spbill_create_ip'],
-                            sign=self.pay_data['sign'])
+
+        post_data = {
+            'appid': self.appid,  # 小程序ID
+            'mch_id': self.mch_id,  # 商户号
+            'nonce_str': get_nonce_str(),  # 随机字符串
+            'body': self.pay_data.get('body'),  # 商品描述
+            'out_trade_no': str(int(time.time())),  # 商户订单号
+            'total_fee': self.pay_data.get('total_fee'),  # 订单总金额，单位为分
+            'spbill_create_ip': self.spbill_create_ip,  # 终端 IP
+            'notify_url': self.notify_url,  # 通知地址
+            'trade_type': 'JSAPI',  # 交易类型，小程序 JSAPI
+            'openid': self.pay_data.get('openid')
+        }
+        sign = self.create_sign(post_data)
+        post_data['sign'] = sign
+
+        xml = dict_to_order_xml(post_data)
+
         # 统一下单接口请求
         r = requests.post(self.url, data=xml.encode("utf-8"))
-        prepay_id = xml_to_dict(r.text).get('prepay_id')
-        # 对返回的 xml 解析
-        paySign_data = {
-            'appId': self.pay_data.get('appid'),
-            'timeStamp': self.pay_data.get('out_trade_no'),
-            'nonceStr': self.pay_data.get('nonce_str'),
+        r.encoding = "utf-8"
+        res = xml_to_dict(r.text)
+        err_code_des = res.get('err_code_des')
+        # 出错信息
+        if err_code_des:
+            return {'code': 40001, 'msg': err_code_des}
+        prepay_id = res.get('prepay_id')
+
+        return self.re_sign(post_data, prepay_id)
+
+    def re_sign(self, post_data, prepay_id):
+        """
+        再次对返回的数据签名
+        https://pay.weixin.qq.com/wiki/doc/api/wxa/wxa_api.php?chapter=7_7&index=3
+        :param post_data:
+        :param prepay_id:
+        :return:
+        """
+        pay_sign_data = {
+            'appId': self.appid,  # 注意大小写与统一下单不一致
+            'timeStamp': post_data.get('out_trade_no'),
+            'nonceStr': post_data.get('nonce_str'),
             'package': 'prepay_id={0}'.format(prepay_id),
-            'signType': 'MD5'
+            'signType': 'MD5',
         }
-        # 再次对返回的数据签名
-        paySign = self.create_sign(paySign_data)
-        paySign_data.pop('appId')
-        paySign_data['paySign'] = paySign
-        return paySign_data
+        pay_sign = self.create_sign(pay_sign_data)
+        pay_sign_data['paySign'] = pay_sign
+        return pay_sign_data
